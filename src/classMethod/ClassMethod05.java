a89b281cd1bfa2d0f7c954f2af24237fb47c9903
Demi <gorigoriuhouho0820@gmail.com>
2021-12-29 23:12:40 +0900

    finish AccessModifer

---
+++ src/classMethod/ClassMethod05.java
@@ -0,0 +1,10 @@
+package classMethod;
+
+import classMethod.human.Human05;
+
+public class ClassMethod05 {
+	public static void main(String[] args) {
+		Human05 demi = new Human05();
+		System.out.println("名前は" + demi.getName() + "年齢は" + demi.getAge() + "です");
+	}
+}
